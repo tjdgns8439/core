ebe85ccc73d8e8c6572f5abd994429a5f2985a38
seonghun <tjdgns8439@naver.com>
2023-02-07 15:02:46 +0900

    Create ApplicationContextIntoTest.java
    모든 스프링 빈 조회

---
+++ src/test/java/hello/core/beanfind/ApplicationContextIntoTest.java
@@ -0,0 +1,34 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextIntoTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + "object = " + beanDefinitionName);
+        }
+    }//스프링 내부 빈까지 출력
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION)
+                System.out.println("name = " + beanDefinitionName + "object = " + beanDefinitionName);
+        }
+    }//우리가 등록한 빈만 출력
+}
